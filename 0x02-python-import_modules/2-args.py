f5d835d2890664e35bf1f6c7ad1136b441acb02c
Mahmud Enesi Abdulazeez <abdulazeezm578@gmail.com>
2022-09-23 05:53:54 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 import sys
-if __name__=='__main__':
 
-    if len(sys.argv) == 1:
-        print("0 arguments.")
-    elif len(sys.argv) == 2:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+if __name__ == '__main__':
+    args = sys.argv
+    args.pop(0)
+    argc = len(args)
+    line1 = "{:d} argument".format(argc)
+    if argc == 0:
+        line1 += "s."
+    elif argc == 1:
+        line1 += ":"
     else:
-        print("{:d} arguments:".format(len(sys.argv)-1))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        line1 += "s:"
+    print(line1)
+    for i, arg in enumerate(args):
+        print("{:d}: {:s}".format(i+1, arg))
+
+
+
+
+
+
+
+
+
+
+
+
